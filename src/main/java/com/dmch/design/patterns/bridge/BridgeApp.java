37a5c767760d80d4994b57053bd3be48f86b030d
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-07-19 00:26:22 +0300

    Bridge pattern

---
+++ src/main/java/com/dmch/design/patterns/bridge/BridgeApp.java
@@ -0,0 +1,64 @@
+package com.dmch.design.patterns.bridge;
+
+/**
+ * Created by Dmitrii on 19.07.2018.
+ */
+public class BridgeApp {
+    public static void main(String[] args) {
+        Car sedan = new Sedan(new Kia());
+        sedan.getDetails();
+        //Kia
+        //Sedan
+
+        Car crossover = new Crossover(new Nissan());
+        crossover.getDetails();
+        //Nissan
+        //Crossover
+    }
+}
+interface Make {
+    void getMake();
+}
+class Nissan implements Make{
+    public void getMake() {
+        System.out.println("Nissan");
+    }
+}
+class Kia implements Make{
+    public void getMake() {
+        System.out.println("Kia");
+    }
+}
+class Skoda implements Make{
+    public void getMake() {
+        System.out.println("Skoda");
+    }
+}
+
+abstract class Car{
+    Make make;
+    public Car(Make make) {
+        this.make = make;
+    }
+    abstract void getType();
+    public void getDetails(){
+        make.getMake();
+        getType();
+    }
+}
+class Sedan extends Car{
+    public Sedan(Make make) {
+        super(make);
+    }
+    void getType() {
+        System.out.println("Sedan");
+    }
+}
+class Crossover extends Car{
+    public Crossover(Make make) {
+        super(make);
+    }
+    void getType() {
+        System.out.println("Crossover");
+    }
+}
