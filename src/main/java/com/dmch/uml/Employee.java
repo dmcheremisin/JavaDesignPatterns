4e3f7a916101f19de387c075b03f10eba029fa8e
dmch0916 <dmitry_cher@mail.ru>
2018-08-01 21:00:40 +0300

    some changes about access

--- src/main/java/com/dmch/uml/Employee.java
+++ src/main/java/com/dmch/uml/Employee.java
@@ -52,4 +52,28 @@ public class Employee extends Man {
     public void deletePastPosition(PastPosition pastPosition){
         pastPositions.remove(pastPosition);
     }
+
+    private class PastPosition {
+        private String name;
+        private Department department;
+
+        public PastPosition(String name, Department department) {
+            this.name = name;
+            this.department = department;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public Department getDepartment() {
+            return department;
+        }
+        public void setDepartment(Department department) {
+            this.department = department;
+        }
+    }
+
 }
